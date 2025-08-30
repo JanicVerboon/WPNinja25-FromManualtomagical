c42198c03cfbb3ed4b1c000077c62bc536516814
Loris Ambrozzo <50479061+lorisAmbrozzo@users.noreply.github.com>
2025-08-30 17:45:02 +0200

    Update LogicApp-Permission.ps1

--- Logic App/LogicApp-Permission.ps1
+++ Logic App/LogicApp-Permission.ps1
@@ -2,13 +2,13 @@ Connect-MgGraph -Scopes "AppRoleAssignment.ReadWrite.All", "Application.Read.All
 $managedIdentityObjectId = "<Managed Identity Object ID>"
 
 $permissions = "Machine.ReadWrite.All"
-$graphApi = Get-MgServicePrincipal -Filter "appId eq '00000003-0000-0000-c000-000000000000'"
+$defenderApi = Get-MgServicePrincipal -Filter "appId eq 'fc780465-2017-40d4-a0c5-307022471b92'"
 $permissions = $graphApi.AppRoles | Where-Object { $_.Value -in $permissions -and $_.AllowedMemberTypes -contains "Application" }
 $permissions | ForEach-Object {
     $appRoleAssignment = @{
         ServicePrincipalId = $managedIdentityObjectId
         PrincipalId        = $managedIdentityObjectId
-        ResourceId         = $graphApi.Id 
+        ResourceId         = $defenderApi.Id 
         AppRoleId          = $PSItem.Id 
     }
     New-MgServicePrincipalAppRoleAssignment @appRoleAssignment
